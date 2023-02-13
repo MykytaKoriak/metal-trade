a555414147f8371de2efe344cb6675d6d7814f02
MykytaKoriak <nikitakoriak@gmail.com>
2023-02-13 08:41:19 +0200

    Fixes for about paralax

--- js/src/components/about.js
+++ js/src/components/about.js
@@ -1,15 +1,16 @@
 import $ from "jquery";
 
 if ($('.about-image').length) {
-    var parallax = $(".about-image"),
-        speed = 0.5;
+    var parallax = $(".about-image");
 
-    $(window).scroll(function(){
-        parallax.each(function(el,i){
+    $(window).unbind("scroll").scroll(function () {
+        var wS = $(window).scrollTop();
+        if (wS > parallax.position().top - parallax.height()/2 && wS < (parallax.position().top + parallax.height())) {
+            console.log((window.scrollY - parallax.position().top / 2) * -0.1 + "px");
+            console.log((parallax.position().top - window.scrollY) * -0.1 );
+            console.log(window.scrollY + " " + parallax.position().top);
+            parallax.css("backgroundPosition", "50% " + (parallax.position().top - window.scrollY - 100) * -0.2 + "px");
 
-            var windowYOffset = $(window).scrollTop();
-            $(this).css("backgroundPosition", "50% " + (windowYOffset * speed) + "px");
-
-        });
+        }
     });
 }
