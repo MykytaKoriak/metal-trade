b6739fc51a0d507c477fd503f96d570ec5948f42
MykytaKoriak <nikitakoriak@gmail.com>
2023-02-14 15:50:44 +0200

    Removed console log

--- js/src/components/about.js
+++ js/src/components/about.js
@@ -5,12 +5,8 @@ if ($('.about-image').length) {
 
     $(window).unbind("scroll").scroll(function () {
         var wS = $(window).scrollTop();
-        if (wS > parallax.position().top - parallax.height()/2 && wS < (parallax.position().top + parallax.height())) {
-            console.log((window.scrollY - parallax.position().top / 2) * -0.1 + "px");
-            console.log((parallax.position().top - window.scrollY) * -0.1 );
-            console.log(window.scrollY + " " + parallax.position().top);
+        if (wS > parallax.position().top - parallax.height() / 2 && wS < (parallax.position().top + parallax.height())) {
             parallax.css("backgroundPosition", "50% " + (parallax.position().top - window.scrollY - 100) * -0.2 + "px");
-
         }
     });
 }
