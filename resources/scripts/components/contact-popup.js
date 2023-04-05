c03241d07ca38cd803cd7c22ffb8099f259f99b6
MykytaKoriak <nikitakoriak@gmail.com>
2023-04-05 21:18:07 +0300

    Fixes for contact page and fixed header blocks

--- resources/scripts/components/contact-popup.js
+++ resources/scripts/components/contact-popup.js
@@ -1,9 +1,18 @@
 import $ from "jquery";
 
-$('.contact-page-mail-button').on('click', function (e){
-    $('.contact-page-mail-popup').addClass('active')
-})
+$('.contact-page-mail-button').on('click', function (e) {
+    $('.contact-page-mail-popup').addClass('active');
 
-$('.contact-page-mail-popup-close').on('click', function (e){
+    if ($('.contact-page-mail-popup').hasClass('active')) {
+
+        $(document).on('click', function (e) {
+            if ($(e.target).hasClass('contact-page-mail-popup')) {
+                $('.contact-page-mail-popup').removeClass('active')
+            }
+        })
+    }
+});
+
+$('.contact-page-mail-popup-close').on('click', function (e) {
     $('.contact-page-mail-popup').removeClass('active')
-})
+});
